b46d120895f4b78578c1836e7544cf32efcd1a4c
COJOC <cojoc07@gmail.com>
2019-12-14 10:08:18 +0000

    initial commit

---
+++ src/main/java/com/facultate/magi/magazinonline/MagazinOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.facultate.magi.magazinonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazinOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazinOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facultate/magi/magazinonline/MagazinOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facultate.magi.magazinonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazinOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
